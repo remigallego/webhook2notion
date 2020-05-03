e708b3dc91b1ebaac461a1dbad6c3bf4940c6947
Remi <remi.gallego@hybridheroes.de>
2020-05-03 16:46:39 +0800

    feat: revert end

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 # -*- coding: utf-8 -*-
 import os
-from datetime import datetime
+from datetime import datetime, date
 from notion.collection import NotionDate
 from notion.client import NotionClient
 from math import pi
@@ -36,3 +36,9 @@ def create_todo():
     createNotionTask(token_v2, url, todo)
     return f'✅ Added {todo.title} to Notion!'
 
+
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
