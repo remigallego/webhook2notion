f3cf5837dbde49161e886e5fcede172c5e95bd66
Remi <remi.gallego@hybridheroes.de>
2020-05-08 21:34:01 +0800

    feat: add get_collection

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 # -*- coding: utf-8 -*-
 import os
+import json
 from datetime import datetime, date
 from notion.collection import NotionDate
 from notion.client import NotionClient
@@ -9,8 +10,12 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
+token = os.environ['NOTION_TOKEN']
+client = NotionClient(token)
 
-class Todo:
+"""https://www.notion.so/remigallego/be7420b21a814bdeb93dcd2f1f99615e?v=772f2dabaac541c38761e3b288f163d3"""
+
+""" class Todo:
   title = ''
   date = ''
 
@@ -28,18 +33,24 @@ def createNotionTask(token, collectionURL, content):
     row.title = content.title
     row.Date = NotionDate(content.date)
     row.Status = "Not Started"
-    
-@app.route('/create_todo', methods=['GET'])
+     """
+
+@app.route('/get_collection', methods=['GET'])
+def get_collection():
+    url = request.args.get('url')
+    cv = client.get_collection_view(url)
+    props = cv.collection.get_schema_properties()
+    return json.dumps(props)
+
+""" @app.route('/create_todo', methods=['POST'])
 def create_todo():
-    todo = construct_todo(request.args.get('todo'), request.args.get('date'))
-    token_v2 = request.args.get('token')
-    url = os.environ.get("URL")
-    createNotionTask(token_v2, url, todo)
-    return f'✅ Added {todo.title} to Notion!'
 
-
-
+    values = request.values
+    for key in values.keys():
+        print(values.get(key))
+    return f"{values}"
+ """
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='localhost', port=port, threaded=True)
