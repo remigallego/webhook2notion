e22336dbbf799fe00542cbe892e22f0c0ab17472
Remi <remi.gallego@hybridheroes.de>
2020-05-08 21:40:43 +0800

    0.0.0.0

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def create_todo():
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='localhost', port=port, threaded=True)
+    app.run(host='0.0.0.0', port=port, threaded=True)
